5bedf8617a4470131e39ae7430a99226bf7fbe97
Yang Fan <jane.fan1002@gmail.com>
2019-05-08 00:33:48 -0500

    modify 406

--- src/main/java/google/QueueReconstructionByHeight406.java
+++ src/main/java/google/QueueReconstructionByHeight406.java
@@ -1,19 +1,20 @@
 package google;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class QueueReconstructionByHeight406 {
     public int[][] reconstructQueue(int[][] people) {
         Arrays.sort(people, (a, b) -> a[0] == b[0] ? a[1] - b[1] : b[0] - a[0]);
-        int[][] res = new int[people.length][2];
-        for (int i = 0; i < people.length; i++) {
-            int pos = people[i][1];
-            for (int j = i; j > pos; j--) {
-                res[j] = res[j - 1];
-            }
-            res[pos] = people[i];
+
+        List<int[]> res = new ArrayList<>();
+
+        for(int i = 0; i < people.length; i++) {
+            res.add(people[i][1], people[i]);
         }
-        return res;
+
+        return res.toArray(new int[0][0]);
 
     }
 }
